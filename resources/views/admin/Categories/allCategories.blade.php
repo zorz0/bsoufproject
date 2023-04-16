22ac27673f4ddf2b1c6b1a5d57be64408adf0fa9
ahmedwahba333 <ahmedmismail39@gmail.com>
2023-04-16 19:42:03 +0200

    start

--- resources/views/admin/Categories/allCategories.blade.php
+++ resources/views/admin/Categories/allCategories.blade.php
@@ -1,158 +1,162 @@
 @extends('layouts.admin_master')
 @extends('layouts.sidemenu')
 @section('content')
-<!-- BEGIN CONTENT -->
-<!-- BEGIN CONTENT BODY -->
-<div class="page-content">
-    <!-- BEGIN PAGE HEADER-->
-    <!-- BEGIN PAGE BAR -->
-    <div class="page-bar">
-        <ul class="page-breadcrumb">
-            <li>
-                <a href="index.html">الرئيسية</a>
-                <i class="fa fa-circle"></i>
-            </li>
-            <li>
-                <a href="#">الفئات</a>
-                <i class="fa fa-circle"></i>
-            </li>
-            <li>
-                <span>جميع الفئات</span>
-            </li>
-        </ul>
-    </div>
-    <!-- END PAGE BAR -->
-    <!-- BEGIN PAGE TITLE-->
-    <h3 class="page-title"> جميع الفئات
-        {{-- <small>managed datatable samples</small> --}}
-    </h3>
-    <!-- END PAGE TITLE-->
-    <div class="row">
-        <div class="col-md-12">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="portlet light bordered">
-                        <div class="portlet-title">
-                            <div class="caption">
-                                <i class="icon-settings font-dark"></i>
-                               <a href="{{route('categories.create')}}"> <span class="caption-subject font-dark sbold uppercase">اضافة فئة جديدة</span>
-                               </a>
-                               
-         
+    <!-- BEGIN CONTENT -->
+    <!-- BEGIN CONTENT BODY -->
+    <div class="page-content">
+        <!-- BEGIN PAGE HEADER-->
+        <!-- BEGIN PAGE BAR -->
+        <div class="page-bar">
+            <ul class="page-breadcrumb">
+                <li>
+                    <a href="index.html">الرئيسية</a>
+                    <i class="fa fa-circle"></i>
+                </li>
+                <li>
+                    <a href="#">الفئات</a>
+                    <i class="fa fa-circle"></i>
+                </li>
+                <li>
+                    <span>جميع الفئات</span>
+                </li>
+            </ul>
+        </div>
+        <!-- END PAGE BAR -->
+        <!-- BEGIN PAGE TITLE-->
+        <h3 class="page-title"> جميع الفئات
+            {{-- <small>managed datatable samples</small> --}}
+        </h3>
+        <!-- END PAGE TITLE-->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="portlet light bordered">
+                            <div class="portlet-title">
+                                <div class="caption">
+                                    <i class="icon-settings font-dark"></i>
+                                    <a href="{{ route('categories.create') }}"> <span
+                                            class="caption-subject font-dark sbold uppercase">اضافة فئة جديدة</span>
+                                    </a>
+
+
+                                </div>
+                            </div>
+                            <div class="portlet-body form">
+
+
+
+                                {{-- {{dd($_SERVER['REQUEST_URI'])}} --}}
+
                             </div>
                         </div>
-                        <div class="portlet-body form">
-                          
-                      
-                          
-                           {{-- {{dd($_SERVER['REQUEST_URI'])}} --}}
-
+                    </div>
+                </div>
+                <!-- BEGIN EXAMPLE TABLE PORTLET-->
+                <div class="portlet light bordered">
+                    <div class="portlet-title">
+                        <div class="caption font-dark">
+                            <i class="icon-settings font-dark"></i>
+                            <span class="caption-subject bold uppercase"> الفئات</span>
                         </div>
                     </div>
-                </div>
-            </div>
-            <!-- BEGIN EXAMPLE TABLE PORTLET-->
-            <div class="portlet light bordered">
-                <div class="portlet-title">
-                    <div class="caption font-dark">
-                        <i class="icon-settings font-dark"></i>
-                        <span class="caption-subject bold uppercase"> الفئات</span>
-                    </div>
-                </div>
-                <div class="portlet-body">
-                    <div class="table-toolbar">
-                        <div class="row">
-                            <div class="col-md-6">
-                                {{-- <div class="btn-group">
+                    <div class="portlet-body">
+                        <div class="table-toolbar">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    {{-- <div class="btn-group">
                                     <button id="sample_editable_1_new" class="btn sbold green"> أضافة فئة
                                         <i class="fa fa-plus"></i>
                                     </button>
                                 </div> --}}
-                            </div>
-                            <div class="col-md-6">
-                                <div class="btn-group pull-right">
-                                    <button class="btn green  btn-outline dropdown-toggle" data-toggle="dropdown">أدوات
-                                        <i class="fa fa-angle-down"></i>
-                                    </button>
-                                    <ul class="dropdown-menu pull-right">
-                                        <li>
-                                            <a href="javascript:;">
-                                                <i class="fa fa-print"></i> طباعة </a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:;">
-                                                <i class="fa fa-file-pdf-o"></i> حفظ كـ PDF </a>
-                                        </li>
-                                        <li>
-                                            <a href="javascript:;">
-                                                <i class="fa fa-file-excel-o"></i> استيراد إلى Excel </a>
-                                        </li>
-                                    </ul>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="btn-group pull-right">
+                                        <button class="btn green  btn-outline dropdown-toggle" data-toggle="dropdown">أدوات
+                                            <i class="fa fa-angle-down"></i>
+                                        </button>
+                                        <ul class="dropdown-menu pull-right">
+                                            <li>
+                                                <a href="javascript:;">
+                                                    <i class="fa fa-print"></i> طباعة </a>
+                                            </li>
+                                            <li>
+                                                <a href="javascript:;">
+                                                    <i class="fa fa-file-pdf-o"></i> حفظ كـ PDF </a>
+                                            </li>
+                                            <li>
+                                                <a href="javascript:;">
+                                                    <i class="fa fa-file-excel-o"></i> استيراد إلى Excel </a>
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <table class="table table-striped table-bordered table-hover table-checkable order-column" id="sample_1">
-                        <thead>
-                            <tr>
-                                <th>
-                                    <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
-                                        <input type="checkbox" class="group-checkable" data-set="#sample_1 .checkboxes" />
-                                        <span></span>
-                                    </label>
-                                </th>
-                                <th> اسم الفئة </th>
-                                <th>صورة الفئة</th>
+                        <table class="table table-striped table-bordered table-hover table-checkable order-column"
+                            id="sample_1">
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
+                                            <input type="checkbox" class="group-checkable"
+                                                data-set="#sample_1 .checkboxes" />
+                                            <span></span>
+                                        </label>
+                                    </th>
+                                    <th> اسم الفئة </th>
+                                    <th>صورة الفئة</th>
 
-                                <th> انضم في </th>
-                                <th> اجراءات </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($categories as $category)
-                            <tr class="odd gradeX">
-                                <td>
-                                    <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
-                                        <input type="checkbox" class="checkboxes" value="1" />
-                                        <span></span>
-                                    </label>
-                                </td>
-                                <td> {{$category->name}}</td>
-                                <td>
-                                <img src="/storage/imgs/{{$category->image}}"  width="100px" alt="">
-                                </td>
+                                    <th> انضم في </th>
+                                    <th> اجراءات </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($categories as $category)
+                                    <tr class="odd gradeX">
+                                        <td>
+                                            <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
+                                                <input type="checkbox" class="checkboxes" value="1" />
+                                                <span></span>
+                                            </label>
+                                        </td>
+                                        <td> {{ $category->name }}</td>
+                                        <td>
+                                            <img src="/storage/imgs/{{ $category->image }}" width="100px" alt="">
+                                        </td>
 
-                                <td class="center"> {{$category->created_at}}</td>
-                                <td>
-                                    <div class="btn-group">
-                                        <button class="btn btn-xs green dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false"> اجراءات
-                                            <i class="fa fa-angle-down"></i>
-                                        </button>
-                                        <ul class="dropdown-menu" role="menu">
-                                            <li>
+                                        <td class="center"> {{ $category->created_at }}</td>
+                                        <td>
+                                            <div class="btn-group">
+                                                <button class="btn btn-xs green dropdown-toggle" type="button"
+                                                    data-toggle="dropdown" aria-expanded="false"> اجراءات
+                                                    <i class="fa fa-angle-down"></i>
+                                                </button>
+                                                <ul class="dropdown-menu" role="menu">
+                                                    <li>
 
-                                                    <a href="{{route('categories.edit',['id'=>$category->id])}}" >
-                                                        <i class="icon-docs"></i> تعديل </a>
+                                                        <a href="{{ route('categories.edit', ['id' => $category->id]) }}">
+                                                            <i class="icon-docs"></i> تعديل </a>
 
-                                            </li>
-                                            <li>
+                                                    </li>
+                                                    <li>
 
-                                                    <a href="{{route('categories.destroy',['id'=>$category->id])}}" >
-                                                        <i class="icon-docs"></i> حذف  </a>
+                                                        <a href="{{ route('categories.destroy', ['id' => $category->id]) }}">
+                                                            <i class="icon-docs"></i> حذف </a>
 
-                                            </li>
-                                            {{-- <li>
+                                                    </li>
+                                                    {{-- <li>
                                                 <a href="javascript:;">
                                                     <i class="icon-trash"></i> حذف </a>
                                             </li> --}}
-                                        </ul>
-                                    </div>
-                                </td>
-                            </tr>
-@endforeach
-{{$categories->links()}}
-                            {{-- they will come from database --}}
-                            {{-- <tr class="odd gradeX">
+                                                </ul>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                {{ $categories->links() }}
+                                {{-- they will come from database --}}
+                                {{-- <tr class="odd gradeX">
                                 <td>
                                     <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
                                         <input type="checkbox" class="checkboxes" value="1" />
@@ -944,18 +948,18 @@
                                     </div>
                                 </td>
                             </tr> --}}
-                            {{-- end --}}
-                        </tbody>
-                    </table>
+                                {{-- end --}}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
+                <!-- END EXAMPLE TABLE PORTLET-->
             </div>
-            <!-- END EXAMPLE TABLE PORTLET-->
         </div>
     </div>
-</div>
-<!-- END CONTENT BODY -->
-<!-- END CONTENT -->
+    <!-- END CONTENT BODY -->
+    <!-- END CONTENT -->
 @endsection
 @push('custom-scripts')
-{{-- <script src="{{asset('/assets')}}/pages/scripts/table-datatables-managed.min.js" type="text/javascript"></script> --}}
+    {{-- <script src="{{asset('/assets')}}/pages/scripts/table-datatables-managed.min.js" type="text/javascript"></script> --}}
 @endpush

73777b802a0cb257235aa9d691d1aeeba619ae5e
AnfalMohamedIbrahimAhmed <anfal.mohamed38@gmail.com>
2023-04-17 05:30:27 +0200

    Dashboard

--- routes/admin.php
+++ routes/admin.php
@@ -55,8 +55,7 @@ Route::get('/users/delete/{id}', [UserController::class, 'destroy'])->name('user
 
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-// Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-
+Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
 Route::post('/dashboard/categories', [CategoryController::class, 'store'])->name('categories.store');
 Route::get('/dashboard/categories/edit/{id}', [CategoryController::class, 'edit'])->name('categories.edit');
 Route::post('/dashboard/categories/update/{id}', [CategoryController::class, 'update'])->name('categories.update');
